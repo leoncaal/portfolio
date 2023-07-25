79759ecf6d9c4a468ec9e63f8bc96126ac04db5b
leoncaal <leonel_1mx@yahoo.com.mx>
2023-07-25 14:17:22 -0600

    package2

--- src/components/googleAnalytics/GoogleAnalytics.js
+++ src/components/googleAnalytics/GoogleAnalytics.js
@@ -25,10 +25,6 @@ export default function GoogleAnalytics(GA_MEASUREMENT_ID){
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-
-                gtag('consent', 'default', {
-                    'analytics_storage': 'denied'
-                });
                 
                 gtag('config', '${GA_MEASUREMENT_ID}', {
                     page_path: window.location.pathname,
